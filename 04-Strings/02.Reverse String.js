43e43da7ae1c44329164d0d4bdb06b2f5f91c9a0
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-07-08 08:39:16 +0530

    reverse string

---
+++ 04-Strings/02.Reverse String.js
@@ -0,0 +1,8 @@
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+function reverseString(strArray) {
+  return strArray.reverse();
+}
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
